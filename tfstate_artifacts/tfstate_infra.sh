f86253ca1c9fd91f58f7e1466657d562c0da64f0
yoii18 <dpssalhotra@gmail.com>
2026-04-29 13:11:57 +0530

    did everything till service principal

--- tfstate_artifacts/tfstate_infra.sh
+++ tfstate_artifacts/tfstate_infra.sh
@@ -20,10 +20,24 @@ if ! az storage account show --name "$STRG_ACCT_NAME" --resource-group "$TF_RG_N
         --hns true
 fi
 
-if [ "$(az storage container exists --name staging --account-name "$STRG_ACCT_NAME" --query exists -o tsv)" = "false" ]; then
-    az storage container create --name staging --account-name "$STRG_ACCT_NAME"
+if [ "$(az storage container exists --name staging --account-name "$STRG_ACCT_NAME" --auth-mode login --query exists -o tsv)" = "false" ]; then
+    az storage container create --name staging --account-name "$STRG_ACCT_NAME" --auth-mode login
 fi
 
-if [ "$(az storage container exists --name prod --account-name "$STRG_ACCT_NAME" --query exists -o tsv)" = "false" ]; then
-    az storage container create --name prod --account-name "$STRG_ACCT_NAME"
+if [ "$(az storage container exists --name prod --account-name "$STRG_ACCT_NAME" --auth-mode login --query exists -o tsv)" = "false" ]; then
+    az storage container create --name prod --account-name "$STRG_ACCT_NAME" --auth-mode login
 fi
+
+SUB_ID=$(az account show --query id -o tsv)
+TENANT_ID=$(az account show --query tenantId -o tsv)
+
+STAGING_APP=$(az ad app create --display-name "staging-app" --query appId -o tsv)
+az ad sp create --id "$STAGING_APP"
+
+PROD_APP=$(az ad app create --display-name "prod-app" --query appId -o tsv)
+az ad sp create --id "$PROD_APP"
+
+echo "AZURE_TENANT_ID          = $TENANT_ID"
+echo "AZURE_SUBSCRIPTION_ID    = $SUB_ID"
+echo "AZURE_CLIENT_ID_STAGING  = $STAGING_APP"
+echo "AZURE_CLIENT_ID_PROD  = $PROD_APP"
