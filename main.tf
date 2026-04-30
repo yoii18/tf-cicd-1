8bfbececc492ec67ba86479a2c52b93986526199
yoii18 <dpssalhotra@gmail.com>
2026-04-30 13:13:07 +0530

    xyz

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 
 module "storage" {
   source               = "./modules/storage-account"
-  location             = var.location
-  rg_name              = var.rg_name
+  location             = azurerm_resource_group.rg.location
+  rg_name              = azurerm_resource_group.rg.name
   storage_account_name = var.storage_account_name
 }
