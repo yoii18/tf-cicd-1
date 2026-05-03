e6e95c0097fe1cecb3f9292fb81b17aebd616c38
yoii18 <dpssalhotra@gmail.com>
2026-05-03 21:38:23 +0530

    changed the adls linked service

--- modules/data-factory/main.tf
+++ modules/data-factory/main.tf
@@ -33,12 +33,34 @@ resource "azurerm_data_factory_linked_custom_service" "rest_api_ls" {
   })
 }
 
-resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "adls_ls" {
-  name                 = "adls_ls"
-  data_factory_id      = azurerm_data_factory.adf.id
-  use_managed_identity = true
-  url                  = "https://${var.storage_account_name}.dfs.core.windows.net"
+resource "azurerm_data_factory_linked_custom_service" "adls_ls" {
+  name            = "adls_ls"
+  type            = "AzureBlobFS"
+  data_factory_id = azurerm_data_factory.adf.id
+
+  type_properties_json = jsonencode({
+    url = "https://${var.storage_account_name}.dfs.core.windows.net"
+    credential = {
+      referenceName = azurerm_data_factory_credential_user_managed_identity.uami_credential.name
+      type          = "CredentialReference"
+    }
+  })
 }
+
+resource "azurerm_data_factory_credential_user_managed_identity" "uami_credential" {
+  name            = "uami-credential"
+  data_factory_id = azurerm_data_factory.adf.id
+  identity_id     = azurerm_user_assigned_identity.adf_uai.id
+}
+
+# resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "adls_ls" {
+#   name            = "adls_ls"
+#   data_factory_id = azurerm_data_factory.adf.id
+#   #   use_managed_identity = true
+#   service_principal_id = azurerm_user_assigned_identity.adf_uai.client_id
+#   tenant               = data.azurerm_client_config.current.tenane_id
+#   url                  = "https://${var.storage_account_name}.dfs.core.windows.net"
+# }
 
 resource "azurerm_data_factory_custom_dataset" "rest_api_ds" {
   name            = "rest_api_ds"
@@ -57,7 +79,7 @@ resource "azurerm_data_factory_custom_dataset" "rest_api_ds" {
 resource "azurerm_data_factory_custom_dataset" "adls_ds" {
   name            = "adls_dataset"
   data_factory_id = azurerm_data_factory.adf.id
-  type            = "AzureBlobFSFile"
+  type            = "Json"
   linked_service {
     name = azurerm_data_factory_linked_service_data_lake_storage_gen2.adls_ls.name
   }
